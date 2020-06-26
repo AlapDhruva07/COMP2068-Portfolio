c9bb58c651cb9e3d20b8bc1c8870b061859d2221
Alap Dhruva <alapdhruva1996@gmail.com>
2020-06-26 13:04:26 -0400

    About page

--- COMP2068_Assignment1_Portfolio/routes/index.js
+++ COMP2068_Assignment1_Portfolio/routes/index.js
@@ -7,4 +7,8 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function (req, res) {
+    res.render('about', { title: 'About Page' });
+});
+
 module.exports = router;
