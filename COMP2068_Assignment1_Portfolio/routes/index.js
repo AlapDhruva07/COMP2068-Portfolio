1d9ee21ea9925bbdc03f37c6c711178103e115dd
Alap Dhruva <alapdhruva1996@gmail.com>
2020-07-02 20:30:34 -0400

    final-touch

--- COMP2068_Assignment1_Portfolio/routes/index.js
+++ COMP2068_Assignment1_Portfolio/routes/index.js
@@ -7,18 +7,22 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
+/* GET about page. */
 router.get('/about', function (req, res) {
     res.render('about', { title: 'About Page' });
 });
 
+/* GET project page. */
 router.get('/project', function (req, res) {
     res.render('project', { title: 'Project Page' });
 });
 
+/* GET contact page. */
 router.get('/contact', function (req, res) {
     res.render('contact', { title: 'Contact Page' });
 });
 
+/* GET service page. */
 router.get('/service', function (req, res) {
     res.render('service', { title: 'Service Page' });
 });
